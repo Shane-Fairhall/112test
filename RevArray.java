4aa4cb6ee31a271381f80a0b3392a76d26bf1170
Shane-Fairhall <168511093+Shane-Fairhall@users.noreply.github.com>
2024-05-12 17:52:34 +1200

    Second version
    Some files changed (e.g. reverseArray) , others didn't

--- RevArray.java
+++ RevArray.java
@@ -5,31 +5,41 @@
  * Shane Fairhall
  * @version (a version number or a date)
  */
+
 public class RevArray
 {
+
+    /**
+     * reverseArray takes an array and rearranges its contents
+     * Accepts a string array as an argument
+     * Returns a string array
+     *
+     * Shane Fairhall
+     * updated 12/05/24
+     */
     static String[] reverseArray(String[] list){
         int len = list.length;
         String[] newList = new String[len];
-        
+
         for(int i=0; i<len; i++){
-        
+
             newList[i] = list[len-(i+1)];
-        
+
         }
-        
+
         return(newList);
     }
-    
+
     public static void main(String[] args)
     {
         String[] names = {"Bob", "Sue", "Jim", "Jo"};
-        
+
         String[] newNames;
         newNames = reverseArray(names);
-        
+
         for(String name : newNames){
             System.out.println(name);
         }
-        
+
     }
 }
--- rounding.java
+++ rounding.java
@@ -9,6 +9,7 @@ public class rounding
 {
     public static void main(String[] args)
     {
+        /*This is an example of how to round using .format*/
         double pi = 3.14159265359;
         System.out.format("Pi to 2 decimal places is %.2f", pi);
     }
